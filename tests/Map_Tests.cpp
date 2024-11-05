c53bac58e84c81ce1479f67668686c3be9b38944
Skyler He <skylerhe@MacBook-Pro-5.richmond.edu>
2024-11-05 16:26:26 -0500

    tests

--- tests/Map_Tests.cpp
+++ tests/Map_Tests.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include "../sim_lib/map.hpp"
+#include "../include/sim_lib/map.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <catch2/catch_approx.hpp>
