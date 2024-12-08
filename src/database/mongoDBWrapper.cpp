9ab8163adfd578d089dd78c9478652124fd2b88a
Skyler He <106091040+SkylerHe@users.noreply.github.com>
2024-12-08 16:17:29 -0500

    Update mongoDBWrapper.cpp

--- src/database/mongoDBWrapper.cpp
+++ src/database/mongoDBWrapper.cpp
@@ -125,7 +125,7 @@ nlohmann::json MongoDBWrapper::getAllDataAsJson(const std::string& collectionTyp
             spdlog::error("Invalid collection type: {}", collectionType);
             return nlohmann::json(); // Return empty JSON for invalid collection type
         }
-        nlohmann::json allData = nlohmann::json::array();  // Initialize an empty array to store all data
+        
 
 
         // Static variable to track whether data has been retrieved before
@@ -141,7 +141,7 @@ nlohmann::json MongoDBWrapper::getAllDataAsJson(const std::string& collectionTyp
         // Retrieve all documents in the collection
         auto cursor = collection->find({});  // Empty filter to find all documents
         
-        
+        nlohmann::json allData = nlohmann::json::array();  // Initialize an empty array to store all data
         for (const auto& document : cursor) {
             // Convert BSON to JSON
             nlohmann::json documentData = nlohmann::json::parse(bsoncxx::to_json(document));
