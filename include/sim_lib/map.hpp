ed7c705c33ffe7de60b23d706dfabaf53ce67d2a
Dhruvam-Desai <dhruvam.desai@richmond.edu>
2024-12-09 00:13:54 -0500

    Comment and clean map

--- include/sim_lib/map.hpp
+++ include/sim_lib/map.hpp
@@ -2,12 +2,9 @@
 #define MAP_HPP
 
 #include <string>
-#include <map>
-#include <vector>
 #include <nlohmann/json.hpp>
-using json = nlohmann::json;
 
-// Enum Classes
+// Enum Class for defining the floor types
 enum class FloorType{
     Wood, Tile, Carpet
 };
@@ -15,27 +12,27 @@ enum class FloorType{
 class Map{
     private:
         std::string name; // Name of map object
-        json rooms;
-        int numRooms;
-        //std::map<std::string, std::vector<std::string>> rooms; //temp vector
+        nlohmann::json rooms; // JSON representation of the rooms and the map
+        int numRooms; // Number of rooms in the map
 
     public:
-        //Map(std::string name, std::map<std::string, std::vector<std::string>> rooms);
         Map();
-        Map(std::string name, json rooms);
-        std::string getName() { return name; };
-        void setName(std::string mapName) { name = mapName; };
+        Map(std::string name, nlohmann::json rooms);
 
-        void updateRoomCleanliness(std::string roomId, std::string cleaningStatus); // Updates cleaning status of given room **string cleanliness -> int later
-        std::string getRoomCleanliness(std::string roomId) { return rooms[roomId]["Cleaning Status"]; }
+        std::string getName() { return name; }; // Getter for name of the map
+        void setName(std::string mapName) { name = mapName; }; // Setter for name of the map
+
+        void updateRoomCleanliness(std::string roomId, std::string cleaningStatus);
+        std::string getRoomCleanliness(std::string roomId) { return rooms[roomId]["Cleaning Status"]; } // Gets the cleaning status of the room
         
         void updateRoomName(std::string roomId, std::string roomName); // Updates name of given room
-        std::string getRoomName(std::string roomId) { return rooms[roomId]["Room"]; }
+        std::string getRoomName(std::string roomId) { return rooms[roomId]["Room"]; } // Get the name of given room
         bool roomExists(const std::string& roomId) const;
 
-        int getNumRooms() { return numRooms; }
-        std::string getFloorType(std::string roomId) { return rooms[roomId]["FloorType"]; }
-        // Note that there are no methods to update/set floorType.
+        int getNumRooms() { return numRooms; } // Getter for number of rooms
+        std::string getFloorType(std::string roomId) { return rooms[roomId]["FloorType"]; } // Gets the floor type of given room ID
+        
+        // Note: that there are no methods to update/set floorType.
 };
 
 #endif // MAP_HPP
